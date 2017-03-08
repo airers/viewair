144f924e8756c82601fe079168145fde17be066e
Jia Xun <chaijiaxun@gmail.com>
2017-03-08 15:40:09 +0800

    Refactored activities into fragments

--- app/src/main/java/com/chaijiaxun/pm25tracker/MainActivity.java
+++ app/src/main/java/com/chaijiaxun/pm25tracker/MainActivity.java
@@ -1,14 +1,15 @@
 package com.chaijiaxun.pm25tracker;
 
 import android.Manifest;
+
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -17,24 +18,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ListView;
-
-import com.chaijiaxun.pm25tracker.database.SensorReading;
-
-import java.util.ArrayList;
-import java.util.Date;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, HomeFragment.OnFragmentInteractionListener {
 
+    private static final String TAG = "APPMainActivity";
     GPSTracker tracker;
     final int PERMISSION_REQUEST_LOCATION = 1;
     double lat, lon;
 
-    EditText sensorReading;
-    ListView readingList;
+//    EditText sensorReading;
+//    ListView readingList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,14 +37,14 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -58,8 +52,8 @@ public class MainActivity extends AppCompatActivity
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
-        sensorReading = (EditText) findViewById(R.id.sensor_reading);
-        readingList = (ListView) findViewById(R.id.readings_view);
+//        sensorReading = (EditText) findViewById(R.id.sensor_reading);
+//        readingList = (ListView) findViewById(R.id.readings_view);
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
@@ -80,6 +74,20 @@ public class MainActivity extends AppCompatActivity
             lon = tracker.getLongitude();
             Log.d("MainActivity", lat + " " + lon);
         }
+
+        Fragment fragment = new HomeFragment();
+        Bundle args = new Bundle();
+//                args.putInt(HomeFragment.ARG_PLANET_NUMBER, position);
+//                fragment.setArguments(args);
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.content_frame, fragment)
+                .commit();
+
+        Log.d(TAG, "Pressed home");
+
 
 //        loadReadings();
     }
@@ -147,15 +155,27 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
+        Log.d(TAG, "Navigation Item Selected");
+        FragmentManager fragmentManager = getSupportFragmentManager();
         switch (id) {
             case R.id.nav_home:
+                fragmentManager.beginTransaction()
+                        .replace(R.id.content_frame, HomeFragment.newInstance("1231", "123123"))
+                        .commit();
 
+                setTitle("PM2.5 App");
+
+                Log.d(TAG, "Pressed home");
                 break;
             case R.id.nav_stats:
+                setTitle("Statistics");
 
                 break;
             case R.id.nav_map:
+                setTitle("Map");
+                fragmentManager.beginTransaction()
+                        .replace(R.id.content_frame, MapFragment.newInstance("1231", "123123"))
+                        .commit();
 
                 break;
             case R.id.nav_readings:
@@ -177,50 +197,54 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
-
-
-    public void clickLocation(View button) {
-        SensorReading.deleteAll();
-        loadReadings();
-    }
-
-    public void saveReading(View button) {
-        Log.d("MainActivity", "Saving reading");
-        String text = sensorReading.getText().toString();
-        int readingInt;
-        try {
-            readingInt = Integer.parseInt(text);
-        } catch (NumberFormatException e) {
-            readingInt = 0;
-        }
-        Date date = new Date();
-        Log.d("MainActivity", text + " " + lat + " " + lon + " " + date.toString());
-
-        SensorReading reading = new SensorReading(date, readingInt, 0, (float)lat, (float)lon, 0);
-
-        reading.save();
-        Log.d("MainActivity", reading.toString());
-
-        loadReadings();
-    }
-
-    public void loadReadings() {
-        ArrayList<SensorReading> list =(ArrayList)SensorReading.getList();
-        String[] values;
-        Log.d("Load Readings", list.size() + " ");
-        if ( list.size() > 0 ) {
-            values = new String[list.size()];
-            for ( int i = 0; i < list.size(); i++ ) {
-                values[i] = list.get(i).toString();
-            }
-        } else {
-            values = new String[] { "Nothing in database" };
-        }
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, android.R.id.text1, values);
-
-        readingList.setAdapter(adapter);
+    @Override
+    public void onFragmentInteraction(Uri uri) {
 
     }
+
+
+//    public void clickLocation(View button) {
+//        SensorReading.deleteAll();
+//        loadReadings();
+//    }
+
+//    public void saveReading(View button) {
+//        Log.d("MainActivity", "Saving reading");
+//        String text = sensorReading.getText().toString();
+//        int readingInt;
+//        try {
+//            readingInt = Integer.parseInt(text);
+//        } catch (NumberFormatException e) {
+//            readingInt = 0;
+//        }
+//        Date date = new Date();
+//        Log.d("MainActivity", text + " " + lat + " " + lon + " " + date.toString());
+//
+//        SensorReading reading = new SensorReading(date, readingInt, 0, (float)lat, (float)lon, 0);
+//
+//        reading.save();
+//        Log.d("MainActivity", reading.toString());
+//
+//        loadReadings();
+//    }
+
+//    public void loadReadings() {
+//        ArrayList<SensorReading> list =(ArrayList)SensorReading.getList();
+//        String[] values;
+//        Log.d("Load Readings", list.size() + " ");
+//        if ( list.size() > 0 ) {
+//            values = new String[list.size()];
+//            for ( int i = 0; i < list.size(); i++ ) {
+//                values[i] = list.get(i).toString();
+//            }
+//        } else {
+//            values = new String[] { "Nothing in database" };
+//        }
+//
+//        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+//                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+//
+//        readingList.setAdapter(adapter);
+//
+//    }
 }
