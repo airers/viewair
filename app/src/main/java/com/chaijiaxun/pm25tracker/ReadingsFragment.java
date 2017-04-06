e376af213bb714ff5ce66469463ab419ea770ef5
Jia Xun <chaijiaxun@gmail.com>
2017-04-06 14:35:12 +0800

    Anyhow did readings page

--- app/src/main/java/com/chaijiaxun/pm25tracker/ReadingsFragment.java
+++ app/src/main/java/com/chaijiaxun/pm25tracker/ReadingsFragment.java
@@ -48,70 +48,15 @@ public class ReadingsFragment extends Fragment{
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
-        View view = inflater.inflate(R.layout.content_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_readings, container, false);
 
-        final Button saveButton = (Button) view.findViewById(R.id.button_save);
 
-        final Button locateButton = (Button) view.findViewById(R.id.button_locate);
-        locateButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                clickLocation();
-            }
-        });
 
-        final Button dbSeedButton = (Button) view.findViewById(R.id.button_db_seed);
-        dbSeedButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                clickDbSeed();
-            }
-        });
-
-        Spinner spinner = (Spinner) view.findViewById(R.id.spinner_reading_filter);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
-                R.array.microclimate_array, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            /**
-             * Called when a new item is selected (in the Spinner)
-             */
-            public void onItemSelected(AdapterView<?> parent, View view,
-                                       int pos, long id) {
-                String selected = (String) parent.getItemAtPosition(pos);
-                switch (selected){
-                    case "Indoors": loadReadings();
-                        break;
-                    case "Outdoors": loadReadings();
-                        break;
-                }
-                Log.d("OnDateChangeListener", (String) parent.getItemAtPosition(pos));
-                //Toast.makeText(MyActivity.this, "Hello Toast",Toast.LENGTH_SHORT).show();
-                //loadReadings();
-            }
-
-            public void onNothingSelected(AdapterView<?> parent) {
-                // Do nothing, just another required interface callback
-            }
-
-        });
-
-        sensorReading = (EditText) view.findViewById(R.id.sensor_reading);
         readingList = (ListView) view.findViewById(R.id.readings_view);
         loadReadings();
 
         return view;
 
-    }
-
-    public void clickLocation() {
-        SensorReading.deleteAll(SensorReading.class);
-        loadReadings();
-    }
-
-    public void clickDbSeed() {
-        DatabaseSeed dbSeed = new DatabaseSeed();
-        dbSeed.seed(10);
-        loadReadings();
     }
 
     public void loadReadings() {
