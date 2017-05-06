c6528945b6b9146ff622f1f47a1db5a9e3d57c17
Jia Xun <chaijiaxun@gmail.com>
2017-05-06 15:55:29 +0800

    Created fragments for settings and help pages

---
+++ app/src/main/java/com/chaijiaxun/pm25tracker/SettingsFragment.java
@@ -0,0 +1,37 @@
+package com.chaijiaxun.pm25tracker;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Settings for the App
+ */
+
+public class SettingsFragment extends Fragment {
+    public SettingsFragment() {
+
+    }
+
+    public static SettingsFragment newInstance() {
+        SettingsFragment fragment = new SettingsFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        return view;
+    }
+}
