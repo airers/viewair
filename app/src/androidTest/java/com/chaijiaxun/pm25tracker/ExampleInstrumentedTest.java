9f79c6e2c0ffd279b2a49dcccfe881732c999543
Jia Xun <chaijiaxun@gmail.com>
2017-05-06 22:55:42 +0800

    Attempts to get tests working have failed

--- app/src/androidTest/java/com/chaijiaxun/pm25tracker/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/chaijiaxun/pm25tracker/ExampleInstrumentedTest.java
@@ -2,12 +2,23 @@ package com.chaijiaxun.pm25tracker;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.widget.TextView;
 
+import com.chaijiaxun.pm25tracker.utils.AppData;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.w3c.dom.Text;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -22,5 +33,19 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.chaijiaxun.pm25tracker", appContext.getPackageName());
+    }
+
+    @Rule
+    public ActivityTestRule<MainActivity> rule  = new  ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void ensureTextViewIsPresent() throws Exception {
+        AppData.getInstance().init(InstrumentationRegistry.getTargetContext());
+        
+        MainActivity activity = rule.getActivity();
+        View messageText = activity.findViewById(R.id.text_message);
+        assertThat(messageText, notNullValue());
+        assertThat(messageText, instanceOf(TextView.class));
+
     }
 }
