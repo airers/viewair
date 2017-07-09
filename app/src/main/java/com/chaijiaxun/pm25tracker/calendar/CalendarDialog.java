7fa1ba17130c491b6bc427b2e006fa4344b59180
Shalom Quek <shalomquek@gmail.com>
2017-07-09 23:30:25 +0800

    Calendar Popup able to select 'today' and 'cancel' #40

--- app/src/main/java/com/chaijiaxun/pm25tracker/calendar/CalendarDialog.java
+++ app/src/main/java/com/chaijiaxun/pm25tracker/calendar/CalendarDialog.java
@@ -3,12 +3,16 @@ package com.chaijiaxun.pm25tracker.calendar;
 
 import android.support.v4.app.DialogFragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.chaijiaxun.pm25tracker.R;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
+import java.util.Calendar;
 
 /**
  * Created by Shalom Quek on 8/7/2017.
@@ -16,6 +20,7 @@ import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 
 public class CalendarDialog extends DialogFragment{
     MaterialCalendarView cv;
+    Button cancelButton, todayButton, selectButton;
 
     public CalendarDialog(){
         // Empty constructor required for DialogFragment
@@ -26,12 +31,34 @@ public class CalendarDialog extends DialogFragment{
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_calendar, container, false);
         cv = (MaterialCalendarView) v.findViewById(R.id.calendarView);
-        //View view = inflater.inflate(R.layout.fragment_edit_name, container);
-        //mEditText = (EditText) view.findViewById(R.id.txt_your_name);
-        //getDialog().setTitle("Hello");
+        cancelButton = (Button)v.findViewById(R.id.button_cancel);
+        todayButton = (Button)v.findViewById(R.id.button_today);
+        selectButton = (Button)v.findViewById(R.id.button_select);
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getDialog().dismiss();
+            }
+        });
+
+        todayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Calendar Picker", "today pressed");
+                cv.setDateSelected(Calendar.getInstance(), true);
+            }
+        });
+
+        selectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Calendar Picker", "select pressed");
+                cv.setCurrentDate(Calendar.getInstance());
+            }
+        });
+
 
         return v;
     }
-
-
 }
